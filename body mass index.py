52ba960e9aab08ac911720cb111ae5073d4cabaf
Sadid <76834719+Sadid-USN@users.noreply.github.com>
2021-01-20 23:12:23 +0300

    Create body mass index.py
    b_m_index

---
+++ body mass index.py
@@ -0,0 +1,15 @@
+name1 = "Shaquille Rashaun O'Neal "
+height1 = 2.16
+weight1 = 45
+
+def body_mass_index(name, height, weight):
+    counting = weight / (height**2)
+    print("body mass index: " + name + str(counting))
+    if counting <25:
+        return name + "Is not overweight"
+    else:
+        return name + "Имеет лиший вес, передожирение "
+
+Shaq = body_mass_index(name1,height1,weight1)
+
+print(Shaq)
