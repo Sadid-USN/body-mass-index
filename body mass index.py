d9dea4ee1b5bbf43e7da843b9ef4c8d78bd84eef
Sadid <76834719+Sadid-USN@users.noreply.github.com>
2021-01-20 23:16:31 +0300

    Update body mass index.py

--- body mass index.py
+++ body mass index.py
@@ -8,7 +8,7 @@ def body_mass_index(name, height, weight):
     if counting <25:
         return name + "Is not overweight"
     else:
-        return name + "Overweight, pre-obese "
+        return name + "is overweight "
 
 Shaq = body_mass_index(name1,height1,weight1)
 
